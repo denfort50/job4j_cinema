117d2d85ef86579123eab9995144e942e0164856
denfort50 <denfort50@yandex.ru>
2022-11-02 15:59:14 +0300

    Добавление Liquibase

---
+++ src/main/resources/db/scripts/003_ddl_create_tickets_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS tickets (
+    id SERIAL PRIMARY KEY,
+    session_id INT NOT NULL REFERENCES sessions(id),
+    pos_row INT NOT NULL,
+    cell INT NOT NULL,
+    user_id INT NOT NULL REFERENCES users(id),
+    CONSTRAINT busy_place UNIQUE (session_id, pos_row, cell)
+);
